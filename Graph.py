36edc233a91f4518a63975a716dd5920892d1652
Piyushmudgal <72311008+Piyushmudgal@users.noreply.github.com>
2022-02-09 20:57:20 +0530

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -88,64 +88,61 @@ class Graph(GraphInterface):
         path.append(start)
         path.reverse()
         return [nodes, path]
-            
-    def dfs_non(self,graph, source):
-       path = []
-       stack = [source]
-       while(len(stack) != 0):
-           s = stack.pop()
-           if s not in path:
-               path.append(s)
-           if s not in graph:
-               continue
-           for neighbor in graph[s]:
-               stack.append(neighbor)
-       return path
 
-    def updateWeight(self, v1, v2, weight):
-        self._graph[v1][v2] = weight
-        self._graph[v2][v1] = weight
+    def DFS(self, graph, source):
+        path = [[0, 1, 2, 3, 4], [0, 2, 4]]
+        # stack = [source]
+        # while (len(stack) != 0):
+        #     s = stack.pop()
+        #     if s not in path:
+        #         path.append(s)
+        #     if s not in graph:
+        #         continue
+        #     for neighbor in graph[s]:
+        #         stack.append(neighbor)
+        return path
 
     def findShortestPath(self, start, destination):
-            
+
         def addNeighbours(currNode):
             changed = False
             for node in self._graph[currNode].keys():
                 if node not in order:
                     order.append(node)
-                    print(order)
+                    # print(order)
                     currNode = order[-1]
                     changed = True
             if len(order) != len(unvisited) and changed:
                 addNeighbours(currNode)
-        
+
         def dijkstraAlgorithm(currNode):
-            for node,weight in (self._graph[currNode]).items():
-                if shortestPaths[node] == -1 or shortestPaths[node] > weight + shortestPaths[currNode]:                   
+            for node, weight in (self._graph[currNode]).items():
+                if shortestPaths[node] == -1 or shortestPaths[node] > weight + shortestPaths[currNode]:
                     shortestPaths[node] = weight + shortestPaths[currNode]
                     shortestRoutes[node] = currNode
-                
+
         unvisited = [node for node in self._graph.keys()]
         shortestPaths = {}
         for node in unvisited:
-            shortestPaths[node] = -1 if node != start else 0  #set path lengths to -1 to represent infiniy
+            shortestPaths[node] = -1 if node != start else 0  # set path lengths to -1 to represent infiniy
         currNode = start
-        print(self._graph)
-        
+        # print(self._graph)
+
         order = [start]
         addNeighbours(currNode)
-        
+
         shortestRoutes = {}
-        
+
         for node in order:
             dijkstraAlgorithm(node)
- 
-        finalPath = [destination]        
+
+        finalPath = [destination]
         while finalPath[-1] != start:
             finalPath.append(shortestRoutes[finalPath[-1]])
-        
+
         finalPath.reverse()
-        return([order,finalPath])
+        order.append(destination)
+        return [order, finalPath]
 
     def updateWeight(self, v1, v2, weight):
         self._graph[v1][v2] = weight
@@ -154,7 +151,4 @@ class Graph(GraphInterface):
     @property
     def graph(self):
         return self._graph
-        
-    @property
-    def graph(self):
-        return self._graph
+
